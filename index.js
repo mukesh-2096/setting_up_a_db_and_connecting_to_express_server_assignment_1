69577e504188ccffd75cec99d3044373bd6a3814
mukesh vantakula <durgasaimukeshvantakula5764@gmail.com>
2025-02-25 09:14:54 +0530

    connect to server

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const express = require('express');
 const { resolve } = require('path');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-// Load environment variables from .env file
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3010;
+const port = 3010;
 
 app.use(express.static('static'));
+
+
+
+const DB_URL = 'mongodb+srv://mukesh:mukesh%40123@cluster-1.6gnqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1';
+
+mongoose.connect(DB_URL)
+.then(()=>{console.log('Mongodb connected Successfully!')})
+.catch(err => console.log('Mongodb connection failed : ',err));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to database'))
-.catch((error) => console.error(`Error connecting to database: ${error}`));
-
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
